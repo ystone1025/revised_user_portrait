b9242828140cd3f1f241ec2e208b256ec3985115
linhaobuaa <1257819385@qq.com>
2016-03-12 17:22:26 +0800

    hxq 0312

---
+++ user_portrait/user_portrait/cron/flow4/scan_topic2sentiment.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+use to save user topic to redis hash for compute sentiment
+update: one week
+'''
+import sys
+
+#use to scan user topic to redis which save as english
+def scan_topic2redis():
+
+
+
+if __name__=='__main__':
+    scan_topic2redis()
