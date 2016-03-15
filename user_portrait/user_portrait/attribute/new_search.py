1051d55388cf15ef96622308f57c73431118fb60
linhaobuaa <1257819385@qq.com>
2016-03-15 21:10:20 +0800

    hxq 0315

--- user_portrait/user_portrait/attribute/new_search.py
+++ user_portrait/user_portrait/attribute/new_search.py
@@ -40,6 +40,54 @@ def new_get_user_profile(uid):
 #use to get tag information/sensitive_words&keywords&hashtag/domain&topic&character
 def new_get_user_portrait(uid, admin_user):
     results = {}
+    try:
+        user_portrait_result = es_user_portrait.get(index=portrait_index_name, doc_type=portrait_index_type,\
+                id=uid)['_source']
+    except:
+        user_portrait_result = {}
+    if not user_portrait_result:
+        results['tag_remark'] = {}
+        results['attention_information'] = {}
+        results['tendency'] = {}
+    else:
+        #step1: get attention_information
+        #sensitive words
+        try:
+            sensitive_words_dict = json.loads(user_portrait_result['sensitive_words_dict'])
+        except:
+            sensitive_words_dict = {}
+        sort_sensitive_words = sorted(sensitive_words_dict.items(), key=lambda x:x[1], reverse=True)
+        results['attention_information'] = {'sensitive_words': sort_sensitive_words}
+        #keywords
+        sort_keywords = json.loads(user_portrait_result['keywords'])
+        results['attention_information']['keywords'] = sorted(sort_keywords)
+        #hashtag
+        hashtag_dict = json.loads(user_portrait_result['hashtag_dict'])
+        sort_hashtag = sorted(hashtag_dict.items(), key=lambda x:x[1], reverse=True)
+        results['attention_information']['hashtag'] = sort_hashtag
+        #step2: get tendency_information
+        results['tendency'] = {'domain':user_portrait_result['domain']}
+        results['tendency']['topic'] = user_portrait_result['topic_string'].split('&')[0]
+        results['tendency']['character_sentiment'] = user_portrait_result['character_sentiment']
+        results['tendency']['character_text'] = user_portrait_result['character_text']
+        #step3: get tag_information
+        #tag
+        try:
+            admin_tag = user_portrait_result[admin_user]
+        except:
+            admin_tag = {}
+        if not admin_tag:
+            results['tag_remark'] = {'tag': []}
+        else:
+            tag_list = admin_tag.split('&')
+            results['tag_remark'] = {'tag': tag_list}
+        #remark
+        try:
+            remark = user_portrait_result['remark']
+        except:
+            remark = ''
+        results['tag_remark']['remark'] = remark
+
     return results
 
 #use to get user evaluate index
