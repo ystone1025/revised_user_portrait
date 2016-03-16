68483d0221c0942090b7a934b91d3bc0de77b84e
linhaobuaa <1257819385@qq.com>
2016-03-16 23:29:29 +0800

    ljh

--- user_portrait/user_portrait/static/js/portrait/search_portrait.js
+++ user_portrait/user_portrait/static/js/portrait/search_portrait.js
@@ -2,15 +2,52 @@ $('#commit_search').click(function(){
     if ($('#supersearch').is(':hidden')){
         var url = '/attribute/portrait_search/?stype=1';
         url += get_simple_par();
-        console.log(url);
-        //base_call_ajax_request(url, draw_search_results);
     }
     else{
         var url = '/attribute/portrait_search/?stype=2';
         url += get_advanced_par();
-        console.log(url);
-        //base_call_ajax_request(url, draw_search_results);
     }
+    console.log(url);
+    draw_conditions(url);
+    //base_call_ajax_request(url, draw_search_results);
 });
 function draw_search_results(data){
+    $('#search_result').empty();
+    var user_url ;
+    //console.log(user_url);
+    var html = '';
+    html += '<table class="table table-striped table-bordered bootstrap-datatable datatable responsive">';
+    html += '<thead><tr><th>用户ID</th><th>昵称</th><th>注册地</th><th>活跃度</th><th>重要度</th><th>影响力</th><th>相关度</th><th>' + '<input name="choose_all" id="choose_all" type="checkbox" value="" onclick="choose_all()" />' + '</th></tr></thead>';
+    html += '<tbody>';
+    for(var i = 0; i<data.length;i++){
+      var item = data[i];
+      item = replace_space(item);
+      for(var j=3;j<7;j++){
+        if(item[j]!='未知')
+          item[j] = item[j].toFixed(2);
+      }
+      global_data[item[0]] = item; // make global data
+      user_url = '/index/personal/?uid=' + item[0];
+      html += '<tr id=' + item[0] +'>';
+      html += '<td class="center" name="uids"><a href='+ user_url+ '  target="_blank">'+ item[0] +'</td>';
+      html += '<td class="center">'+ item[1] +'</td>';
+      html += '<td class="center">'+ item[2] +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[3] +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[4] +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[5] +'</td>';
+      html += '<td class="center" style="width:100px;">'+ item[6] +'</td>';
+      html += '<td class="center"><input name="search_result_option" class="search_result_option" type="checkbox" value="' + item[0] + '" /></td>';
+      html += '</tr>';
+    }
+    html += '</tbody>';
+    html += '</table>';
+    $('#search_result').append(html);
+}
+function replace_space(data){
+  for(var i in data){
+    if(data[i]===""||data[i]==="unknown"){
+      data[i] = "未知";
+    }
+  }
+  return data;
 }
--- user_portrait/user_portrait/static/js/portrait/tbase.js
+++ user_portrait/user_portrait/static/js/portrait/tbase.js
@@ -10,6 +10,88 @@ function bindAdvanced(){
         }
     });
 }
+function draw_conditions(url){
+    $("#search_result").css("margin-top", "40px");
+    $('#conditions').empty();
+    var html = '';
+    var pre_name = 'uname';
+    var pre_value = 'test';
+
+    var fix_result = process_par(pre_name, pre_value);
+    //console.log(fix_result);
+    var fix_name = fix_result[0];
+    var fix_value = fix_result[1];
+    // console.log(fix_name);
+    // console.log(fix_value);
+    if (fix_value){
+        if (fix_value.indexOf(',') >= 0){
+            var term_list = fix_value.split(',');
+            for (var j = 0; j < term_list.length;j++){
+                html += '<span class="mouse" id=choose_' + pre_name + '_' + j +' style="margin-left:10px">'+ fix_name + ':'+ term_list[j];
+                html += '&nbsp;<a class="cross" href="#">X</a></span>';
+            }
+        }
+        else{
+            html += '<span class="mouse" id=choose_' + pre_name + ' style="margin-left:10px">'+ fix_name + ':'+ fix_value;
+            html += '&nbsp;<a class="cross" href="#">X</a></span>';
+        }
+    }
+    $('#conditions').html(html);
+    return;
+}
+function process_par(name, value){
+    var result = new Array();
+    if (name == 'uid'){
+        result[0] = '用户ID';
+        result[1] = value;
+    }
+    else if(name=='uname'){
+        result[0] = '昵称';
+        result[1] = value;
+    }
+    else if(name=='location'){
+        result[0] = '注册地';
+        result[1] = value;
+    }
+    else if(name=='keywords'){
+        result[0] = '关键词';
+        result[1] = value;
+    }
+    else if(name=='hashtag'){
+        result[0] = 'hashtag';
+        result[1] = value;
+    }
+    else if(name=='psycho_status_by_emotion'){
+        result[0] = '性格（语言）';
+        result[1] = value;
+    }
+    else if(name=='psycho_status_by_word'){
+        result[0] = '性格（情绪）';
+        result[1] = value;
+    }
+    else if(name=='domain'){
+        result[0] = '领域';
+        result[1] = value;
+    }
+    else if(name=='topic'){
+        result[0] = '话题';
+        result[1] = value;
+    }
+    else if(name=='tag'){
+        result[0] = '标签';
+        result[1] = '';
+        var term_list = value.split(',');
+        for (var i = 0;i < term_list.length;i++){
+            result[1] += (term_list[i].replace(':', '--') + ',');
+        }
+        result[1] = result[1].substring(0, result[1].length-1);
+    }
+    else{
+        result[0] = '';
+        result[1] = '';
+    }
+    return result;
+}
 function get_simple_par(){
     var str = '&uname=' + $('#uname').val();
     return str
